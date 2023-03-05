a828ff3063084b0ea0bf8a6dce164fadbbf3260a
Ruichen Zhang <ruichen199801@gmail.com>
2023-03-04 22:40:15 -0500

    Initial commit of around (legacy project)

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# around
-A travel photo sharing site using Golang and React
+# Around
+
+Project Date: May 2021
+
+## Description
+
+<img src="./images/demo.png" alt="demo" width="500"><br>
+
+Around is a fullstack web app for sharing photos, videos, and posts. It uses React for the frontend and Go for the backend, and it supports fuzzy search with Elasticsearch.
+
+## Architecture
+
+<img src="./images/architecture.png" alt="architecture" width="500"><br>
+
+### Frontend
+
+Around features user-friendly webpages to create, browse, and search posts with React. Authentication is improved with token-based registration/login/logout and server-side authentication with JWT. The app is deployed on AWS Amplify.
+
+### Backend
+
+Around uses a scalable web service in Go for managing user posts and account information. Elasticsearch provides advanced search features for posts, and the app is deployed on Google App Engine for scalability.
---
+++ backend/elasticsearch.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "context"
+		
+    "github.com/olivere/elastic/v7"
+)
+
+const (
+        ES_URL = "http://10.128.0.2:9200" 
+)
+
+func readFromES(query elastic.Query, index string) (*elastic.SearchResult, error) {
+    client, err := elastic.NewClient(
+        elastic.SetURL(ES_URL),
+        elastic.SetBasicAuth("elastic", "placeholder"))
+    if err != nil {
+        return nil, err
+    }
+
+    searchResult, err := client.Search().
+        Index(index).
+        Query(query).
+        Pretty(true).
+        Do(context.Background())
+    if err != nil {
+        return nil, err
+    }
+
+    return searchResult, nil
+}
+
+func saveToES(i interface{}, index string, id string) error{
+    client, err := elastic.NewClient(
+        elastic.SetURL(ES_URL),
+        elastic.SetBasicAuth("elastic", "placeholder")) 
+    if err != nil {
+        return err
+    }
+
+    _, err = client.Index().
+        Index(index).
+        Id(id).
+        BodyJson(i).
+        Do(context.Background())
+    return err
+}
+
+func deleteFromES(query elastic.Query, index string) error {
+    client, err := elastic.NewClient(
+        elastic.SetURL(ES_URL),
+        elastic.SetBasicAuth("elastic", "placeholder")) 
+    if err != nil {
+        return err
+    }
+
+    _, err = client.DeleteByQuery().
+        Index(index).
+        Query(query).
+        Pretty(true).
+        Do(context.Background())
+
+    return err
+}
